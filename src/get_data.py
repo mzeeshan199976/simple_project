9226de0cc4779befadbd7ec9e2e30c972dff0e24
mzeeshan199976 <mzeeshan199976@gmail.com>
2022-06-11 10:14:16 +0500

    done multiple changes

--- src/get_data.py
+++ src/get_data.py
@@ -1,6 +1,3 @@
-## read params
-## process
-## return dataframe
 import os
 import yaml
 import pandas as pd
@@ -13,10 +10,10 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    print(config)
-    #data_path = config["data_source"]["s3_source"]
-    #df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    #return df
+    #print(config)
+    data_path = config["data_source"]["s3_source"]
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    return df
 
 
 
